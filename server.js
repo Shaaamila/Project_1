e7d8fd6b6c1297ad95c68cd8448b2af3943c094e
Shamila <shamila.m09@gmail.com>
2017-03-11 00:07:50 -0800

    Second commit

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ var app = express();
 // var routeSetter = require('./server/config/routes.js');
 // routeSetter(app);
 
+app.get('/',function (req, res){
+	res.sendfile(__dirname + '/client/views/index.html');
+});
+
+
 app.listen(8000, function() {
 	console.log('The server is listening on port 8000');
 });
